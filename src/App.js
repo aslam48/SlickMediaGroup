9108dcbca20c43c12e6fdaed6592a0822d4790db
aslam48 <youngfizzy001@gmail.com>
2022-12-09 12:16:49 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Component/ui/Header';
+import Main from './Component/ui/main';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Main/>
     </div>
   );
 }
---
+++ src/Component/ui/main.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import '../../App.css'
+import Typewriter  from "typewriter-effect"
+import {motion} from "framer-motion"
+
+
+const main = () => {
+  
+  return (
+    <motion.div
+    initial={{
+        x:-500,
+        opacity:0,
+        scale:0.5
+     }} 
+
+     animate={{
+        x:0,
+        opacity: 1,
+        scale:1,
+     }}
+
+     transition={{
+        duration:1.5,
+     }}
+    className='background-img'>
+
+
+   <motion.div
+   initial={{
+    x:-500,
+    opacity:0,
+    scale:0.5
+ }} 
+
+ animate={{
+    x:0,
+    opacity: 1,
+    scale:1,
+ }}
+
+ transition={{
+    duration:2.5,
+ }}
+    className='background-text'>
+    <p>Watch something  <div className='text-color'> <Typewriter  
+    onInit={(typewriter) => {
+        typewriter
+        .typeString("Fantastic.")
+        .pauseFor(2000)
+        .deleteAll()
+        .typeString("Amazing.")
+        .deleteAll()
+        .typeString("incredible.")
+        .start()
+    }}
+    /> </div></p>
+    
+   </motion.div>
+    </motion.div>
+  )
+}
+
+export default main
