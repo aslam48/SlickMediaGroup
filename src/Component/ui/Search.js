9a3fee549bd9e4defe9b0327541bee542bbc832e
aslam48 <youngfizzy001@gmail.com>
2022-12-09 16:24:50 +0100

    working

--- src/Component/ui/Search.js
+++ src/Component/ui/Search.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../../App.css'
+import { BsSearch } from "react-icons/bs"
 
 
 const Search = () => {
   return (
     <div>
+        <div><BsSearch className='search'/>
         <input placeholder='search movie'/>  
+        </div>
     </div>
   )
 }
