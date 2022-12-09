c941828701e7fc19560d888ba8a996ac0946dd6d
aslam48 <youngfizzy001@gmail.com>
2022-12-09 12:56:38 +0100

    working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './Component/ui/Header';
 import Main from './Component/ui/main';
+import Search from './Component/ui/Search';
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <Main/>
+    <Search/>
     </div>
   );
 }
---
+++ src/Component/ui/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../App.css'
+
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
